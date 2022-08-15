cef640be89a17b0b9b8c04fe8dd0b585a14fd9cf
Henrique Greghi <henriquevsg@gmail.com>
2022-08-15 14:07:56 -0300

    colocando notas

---
+++ Notas/Aluno.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notas
+{
+    class Aluno
+    {
+        public string Nome;
+        public string Endereco;
+        public int Idade;
+        public decimal NotaMatematica;
+        public decimal NotaPortugues;
+        public decimal NotaGeografia;
+
+        public Aluno()
+        {
+        }
+
+        public Aluno(string nome, string endereco, int idade, decimal notaMatematica, decimal notaPortugues, decimal notaGeografia)
+        {
+            Nome = nome;
+            Endereco = endereco;
+            Idade = idade;
+            NotaMatematica = notaMatematica;
+            NotaPortugues = notaPortugues;
+            NotaGeografia = notaGeografia;
+
+        }
+        public void ImprimirNota()
+        {
+          
+
+            Console.WriteLine("Nome : " + Nome);
+            Console.WriteLine("Endereço :" + Endereco);
+            Console.WriteLine("Idade : " + Idade);
+            Console.WriteLine("Nota de Matemática : " + NotaMatematica);
+            Console.WriteLine("Nota de  Português : " + NotaPortugues);
+            Console.WriteLine("Nota de Geografia : " + NotaGeografia);
+        }
+
+        public void ImprimirNota1()
+        {
+          
+
+            
+            Console.WriteLine("Escolha qual matéria deseja imprimir : ");
+            Console.WriteLine("M - Matemática : ");
+            Console.WriteLine("P - Português : ");
+            Console.WriteLine("G - Geografia");
+
+            char nota = char.Parse(Console.ReadLine());
+
+            if (nota == 'M'|| nota =='m')
+            {
+                Console.WriteLine("Nome : " + Nome);
+                Console.WriteLine("Endereço :" + Endereco);
+                Console.WriteLine("Idade : " + Idade);
+                Console.WriteLine("Nota de Matemática : " + NotaMatematica);
+            }
+
+            else if (nota == 'p' || nota == 'P' )
+            {
+                Console.WriteLine("Nome : " + Nome);
+                Console.WriteLine("Endereço :" + Endereco);
+                Console.WriteLine("Idade : " + Idade);
+                Console.WriteLine("Nota de Português : " + NotaPortugues);
+            }
+
+            else if (nota == 'g' || nota == 'G')
+            {
+                Console.WriteLine("Nome : " + Nome);
+                Console.WriteLine("Endereço :" + Endereco);
+                Console.WriteLine("Idade : " + Idade);
+                Console.WriteLine("Nota de Geografia : " + NotaGeografia);
+
+
+            }
+            else
+            {
+                Console.WriteLine("Matéria não encontrada!");
+            }
+
+        }
+    }
+}
+
---
+++ Notas/Program.cs
@@ -0,0 +1,34 @@
+﻿using OpenXmlPowerTools;
+using System;
+
+namespace Notas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+
+            Console.WriteLine("Entre com o nome do aluno : ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Entre com o endereço : ");
+            aluno.Endereco = Console.ReadLine();
+            Console.WriteLine("Entre com a idade do aluno :");
+            aluno.Idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com a nota de matemática : ");
+            aluno.NotaMatematica = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com a nota de português : ");
+            aluno.NotaPortugues = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com a nota de geografia: ");
+            aluno.NotaGeografia = decimal.Parse(Console.ReadLine());
+
+
+            aluno.ImprimirNota();
+            aluno.ImprimirNota1();
+
+
+        }
+                      
+    }
+}
